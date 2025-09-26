940206dc091138d0a83fa8e4d50d484a3fa9c614
snapfix2025 <snapfixsih2025@gmail.com>
2025-09-26 19:25:30 +0530

    Update http.js

--- frontend/src/lib/http.js
+++ frontend/src/lib/http.js
@@ -1,7 +1,17 @@
+// src/lib/http.js
 import axios from "axios";
 import { API_BASE_URL } from "../config";
 
 export const http = axios.create({
   baseURL: API_BASE_URL,
-  // withCredentials: true, // enable if you use cookies/sessions
+  // withCredentials: true, // uncomment if you use cookies/sessions
 });
+
+// Optional: response/error interceptor for consistent errors
+http.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    // You can log or normalize errors here
+    return Promise.reject(err);
+  }
+);
